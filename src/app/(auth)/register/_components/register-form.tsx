a959c27abf6a12c4b3d873d08b89021f496604c7
Jery Hardianto <hardiantojr29@gmail.com>
2025-02-02 15:02:21 +0700

    feat: auth register

--- src/app/(auth)/register/_components/register-form.tsx
+++ src/app/(auth)/register/_components/register-form.tsx
@@ -1,30 +1,58 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Link from "next/link"
+"use client";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export function RegisterForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const { push } = useRouter();
+  const [ error, setError ] = useState("");
+  const [IsLoading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        fullname: e.target.fullname.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    });
+    if (res.status === 200) {
+      e.target.reset();
+      setLoading(false);
+      push("/login");
+    } else {
+      setError("Email already exists");
+      setLoading(false);
+    }
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
           <CardTitle className="text-2xl">Register</CardTitle>
+          {error && (
+            <div className="bg-red-500 p-4 rounded text-white font-bold">
+              {error}
+            </div>
+          )}
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
+              <div className="grid gap-2">
                 <Label htmlFor="fullname">Full Name</Label>
                 <Input
                   id="fullname"
@@ -55,12 +83,12 @@ export function RegisterForm({
             <div className="mt-4 text-center text-sm">
               Already have an account?{" "}
               <Link href="/login" className="underline underline-offset-4">
-                  Sign in
+                Sign in
               </Link>
             </div>
           </form>
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,4 +1,4 @@
-import { RegisterForm } from "@/components/register-form";
+import { RegisterForm } from "@/app/(auth)/register/_components/register-form";
 
 export default function RegisterPage() {
   return (
